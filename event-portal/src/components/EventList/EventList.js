b8c17b7680fd2894f9db060822bd35840caef6a7
jimepholmes <jim_e_p_holmes@hotmail.com>
2019-02-05 09:53:40 +0000

    added some style to events list

--- event-portal/src/components/EventList/EventList.js
+++ event-portal/src/components/EventList/EventList.js
@@ -6,11 +6,17 @@ class EventList extends React.Component{
     render(){
         return (
             <div className="EventList">
+                <div className="EventListHeader">
+                    FEATURED EVENTS
+                </div>
                 {
                     this.props.events.map(event => {
                         return <EventListItem key={event.id} event={event}/>
                     })
                 }
+                <div className="EventListMoreLink">
+                    <a href="">See All ▶</a>
+                </div>
             </div>            
         );
     }
