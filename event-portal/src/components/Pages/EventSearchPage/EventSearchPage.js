252d7998d6c23af2b07ff2a905ee18ffb47cc413
jimepholmes <jim_e_p_holmes@hotmail.com>
2019-02-05 18:03:53 +0000

    added start of routing

---
+++ event-portal/src/components/Pages/EventSearchPage/EventSearchPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './EventSearchPage.css';
+
+class EventSearchPage extends React.Component{
+    render(){
+        return <h1>Event Search Page</h1>
+    }
+}
+
+export default EventSearchPage;
---
+++ event-portal/src/components/Pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './HomePage.css';
+
+class HomePage extends React.Component{
+    render(){
+        return <h1>Home Page</h1>
+    }
+}
+
+export default HomePage;
