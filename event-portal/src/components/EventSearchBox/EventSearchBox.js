893163e95ffbae6dbb798b11595f0a4d79fc1f3e
jimepholmes <jim_e_p_holmes@hotmail.com>
2019-02-06 15:24:55 +0000

    added search box

--- event-portal/src/components/EventSearchBox/EventSearchBox.js
+++ event-portal/src/components/EventSearchBox/EventSearchBox.js
@@ -7,8 +7,21 @@ class EventSearchBox extends React.Component{
         return (
             <div className="EventSearchBox">
                 <div className="SearchControls">
-                    <div className="TitleText">FIND AN EVENT</div>
-                    <SearchButton />
+                    <div className="EventSearchHeader">
+                        FIND AN EVENT
+                    </div>
+                    <div className="EventSearchIntro">
+                        Enter your city, post code, or name of event.
+                    </div>
+                    <div className="EventSearchInputBox">
+                        <div class="inputWithIcon">
+                            <input type="text" placeholder="Search here..." />
+                            <i class="fa fa-search fa-lg fa-fw" aria-hidden="true"></i>
+                        </div>                        
+                    </div>
+                    <div className="SearchButton">
+                        <SearchButton />
+                    </div>
                 </div>
             </div>
         )
