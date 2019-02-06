5d4d60c86461afe9a622db6421b2432726270be9
jimepholmes <jim_e_p_holmes@hotmail.com>
2019-02-06 16:52:40 +0000

    added google map

--- event-portal/src/components/Map/Map.js
+++ event-portal/src/components/Map/Map.js
@@ -4,7 +4,9 @@ import './Map.css';
 class Map extends React.Component{
     render (){
         return (
-            <div className="Map"></div>
+            <div className="Map">
+                <iframe width="100%" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=vauxhall%2C%20london&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            </div>
         )
     }
 }
